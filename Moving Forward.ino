730bb30382ec8a27d63a5a32a9db671fb6baebc3
MasonMichalski <113808882+MasonMichalski@users.noreply.github.com>
2022-09-17 22:04:47 -0400

    Add files via upload

---
+++ Moving Forward.ino
@@ -0,0 +1,18 @@
+//Moving Forward 
+// int pitch
+// int servo CW
+// int servo CCW
+//int depth
+///int depthmin = 2ft(example)
+// int depthmax = 20ft(example)
+//
+//loop {
+  //if (depth=depthmin)
+    // activate servo CW for x amount of time (full time to retract syringes)
+    // check depth 
+    //if (depth=maxdepth)
+      //activate servo CCW for x amount of time ( full time to extend syringes)
+     // check depth
+  //else
+      // check depth
+//}
