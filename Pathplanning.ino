846d763977ca2d67a9b34a64e085feb763b6b0a4
Vinsanity52 <36460322+Vinsanity52@users.noreply.github.com>
2022-11-08 00:11:23 -0500

    Update Pathplanning.ino

--- Pathplanning.ino
+++ Pathplanning.ino
@@ -3,56 +3,69 @@ void setup() {
   int depthmin = 2ft(example);
   int depthmax = 20ft(example);
   float Waypoint = {Lat, Long};
-  float AngleTolerance = {.01,.01};
-  float DistTolerance = .01;
+  float AngleTolerance = {.01,.01};                                     
+  float DistTolerance = 1;
   int command = 0;
+  float targetRoll = 0;
 
 
   //-------------------------------------------------------------------------------------------------------
-  primingSyringes();
-  primingBalast();
-
 
 }
 
 //----------------------------------------------------------------------------------------------------------
 // Main loop
 void loop() {
-  //There should be something here to hold until the go command is sent
-  while(command = 0)
+  //command 0 will wait for a command
+  //command 1 will priming syringes
+  //command 2 will prime balasts
+  //command 3 will start autonomus navigation
+
+  while(command != 3)
   {
     CheckCommands();
+
+      if(command = 1)
+    {
+      primingSyringes();
+    }
+
+    else if(command = 2)
+    {
+      primingBalast();
+    }
+
   }
 
   CheckCommands();
-  readCompass();
-  readGPS();
-  CheckComp_GPS();
+  CheckComp_GPS(Waypoint);
 
-  if(Dist >= DistTolerance)
+  if(Dist > DistTolerance)                                              //If calculated distance is greater than our tollerance run navigation
   {
-    if(abs(AngleCorr) >= AngleTolerance) 
+    if(abs(AngleDiffrence) > AngleTolerance)                            //If the angle the glider is facing is not within our tolerance, correct the angle 
     {
-      AngleCorrection();
-      rollBalance();
+      CheckComp_GPS(Waypoint);                                          // check GPS and compass and calculate distance and angle diffrence
+      AngleCorrection(AngleDiffrence);                                  // Angle Correction to turn the glider 
       moveForward();
-      CheckComp_GPS(Compass, GPS{});
+      CheckComp_GPS(Waypoint);
 
     }
     //once angle is matched proceed forward.
-    else if(abs(AngleCorr) <= AngleTolerance)
+    else if(abs(AngleDiffrence) <= AngleTolerance)
     {
-      rollBalance();
+      targetRoll = 0;
+      CheckComp_GPS(Waypoint);
       moveForward();
-      CheckComp_GPS(Compass, GPS{});
+      RollBalance(targetRoll);
+      CheckComp_GPS(Waypoint);
     }
   }
   else
-  send = false;
+  command = 0;
 
 }
 
-int CheckComp_GPS(GPS{}, Waypoint)
+int CheckComp_GPS(Waypoint)
 {
   readCompass();                                                        // Function that reads the current compass angle reading
   readGPS();                                                            // Function that reads current Lat Long from GPS                                                 
@@ -72,22 +85,31 @@ int CheckComp_GPS(GPS{}, Waypoint)
 
   //Angle difference between compass angle and the angle it should face
   float AngleCorr = {CartCompAng(1) - CartDistAngle(1), CartCompAng(2) - CartDistAngle(2)};
+  float AngleDiffrence = arctan(AngleCorr(2)/AngleCorr(1))
 
-  return AngleCorr; 
+  return AngleDiffrence, Dist, CompassReadout; // I dont think you can return two varriables like this might need to be a struct
 {
 
 
 // -----------------------------------------------------------------------------------------------------------------------
 // Bouancy engine and ballast controlls
 
-void primingSyringes()
+int primingSyringes()
 {
 
+
+  // once priming process is finised return command varriable = 0 
+  command = 0;
+  return command;
 }
 
 void primingbalast()
 {
 
+
+  // once priming process is finised return command varriable = 0 
+  command = 0;
+  return command;
 }
 // this function activates the moving foward loop.
 void moveForward()
@@ -109,10 +131,73 @@ void moveForward()
       // check depth
     //else
         // check depth
+  PitchBalance()
+  RollBalance()
 }
 
-void AngleCorrection()
+void AngleCorrection(AngleDiffrence)
 {
+  yawChange = AngleDiffrence           //Required yaw change
+  if (AngleCorr(2) > 0)                                     //If positive turn left
+  {
+    turningLeft(yawChange);
+  } 
+  else if (AngleCorr(2) < 0)                                // If negative turn Right
+  {
+    turningRight(yawChange);
+  }
+  RollBalance(targetRoll);
+}
+
+float turningLeft(yawChange)
+{
+  // Turning Left Function 
+  
+  if (yawChange > 90)
+  {
+    float targetRoll = 45                           // target roll angle in degrees
+    float RollTolerance = 1;                        // tolerance for roll angle
+    rollDiffrence(targetRoll);
+    while()
+    {
+
+    }    
+  }
+
+
+  // Activate Solenoids 2 and 3
+  //Activate pump for certain time (Large amount)
+  //turn off pump
+  //close solenoids 2 and 3
+
+  // Activate Solenoids 1 and 4
+  //Activate pump for certain time (Large amount same as before)
+  //turn off pump
+  //close solenoids 1 and 4
+  // hold until roll angle is achived with a tolerance 
+
+  //Call to Roll Balance function 
+  return targetRoll; 
+}
+
+void turningRight()
+{
+  //Turning Right Function  
+  //int Rollangle
+
+  // Activate Solenoids 1 and 4
+  //Activate pump for certain time (Large amount same as before)
+  //turn off pump
+  //close solenoids 1 and 4
+  // hold until roll angle is achived with a tolerance 
+
+  // Activate Solenoids 2 and 3
+  //Activate pump for certain time (Large amount)
+  //turn off pump
+  //close solenoids 2 and 3
+
+
+  //Call to Roll Balance function 
 
 }
 
@@ -184,60 +269,17 @@ void rollBalance()
   //turn off pump
   //close solenoids 2 and 3
 }
-
-void turningLeft()
-{
-  // Turning Left Function 
-  int Rollangle;
-
-
-  // Activate Solenoids 2 and 3
-  //Activate pump for certain time (Large amount)
-  //turn off pump
-  //close solenoids 2 and 3
-
-  // Activate Solenoids 1 and 4
-  //Activate pump for certain time (Large amount same as before)
-  //turn off pump
-  //close solenoids 1 and 4
-  // hold until roll angle is achived with a tolerance 
-
-  //Call to Roll Balance function 
-
-}
-
-void turningRight()
-{
-  //Turning Right Function  
-  //int Rollangle
-
-  // Activate Solenoids 1 and 4
-  //Activate pump for certain time (Large amount same as before)
-  //turn off pump
-  //close solenoids 1 and 4
-  // hold until roll angle is achived with a tolerance 
-
-  // Activate Solenoids 2 and 3
-  //Activate pump for certain time (Large amount)
-  //turn off pump
-  //close solenoids 2 and 3
-
-
-  //Call to Roll Balance function 
-
-}
-
 // ------------------------------------------------------------------------------------------------------------
 // Code that utilizes sensors and informaiton gathered from the bouy
 
 
 int readCompass()
 {
-
+  return CompassReadout
 }
 int readGPS()
 {
-  
+  return GPS{}
 }
 
 
@@ -245,9 +287,10 @@ int readGPS()
 //---------------------------------------------------------------------------------------------------------------
 // Code involving on board sensors
 
-int readGyroscope()
+float readGyroscope()
 {
-
+  float GliderRoll                        //Glider's actual roll angle according to Gyroscope
+  return GliderRoll
 }
 
 int readPressure()
@@ -255,11 +298,14 @@ int readPressure()
 
 }
 
-
+rollDiffrence(targetRoll)
+{
+  readGyroscope()
+}
 ------------------------------------------------------------------------------------------------------------------
 // Mobile app communication code
 
 int CheckCommands()
 {
-
+  
 }
